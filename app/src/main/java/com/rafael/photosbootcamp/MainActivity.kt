ef780404d3979a33d6fe63e7736d5acb1bba4bf8
rafaelkenedy <k.rafaelalves@gmail.com>
2022-07-01 15:20:15 -0300

    Finished

--- app/src/main/java/com/rafael/photosbootcamp/MainActivity.kt
+++ app/src/main/java/com/rafael/photosbootcamp/MainActivity.kt
@@ -1,21 +1,28 @@
 package com.rafael.photosbootcamp
 
 import android.Manifest
+import android.annotation.SuppressLint
 import android.app.Activity
+import android.content.ContentValues
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Build
 import android.os.Bundle
+import android.provider.MediaStore
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.rafael.photosbootcamp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    var image_uri: Uri? = null
+
     companion object {
         private val PERMISSION_CODE_IMG_PICK = 1000
         private val IMAGE_PICK_CODE = 1001
         private val PERMISSION_CODE_CAMERA_CAPTURE = 2000
+        private val OPEN_CAMERA_CODE = 2001
     }
 
     private lateinit var binding: ActivityMainBinding
@@ -48,9 +55,12 @@ class MainActivity : AppCompatActivity() {
                     == PackageManager.PERMISSION_DENIED || checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE)
                     == PackageManager.PERMISSION_DENIED
                 ) {
-                    val permissions = arrayOf(Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                    val permissions = arrayOf(
+                        Manifest.permission.CAMERA,
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE
+                    )
                     requestPermissions(permissions, PERMISSION_CODE_CAMERA_CAPTURE)
-                }else{
+                } else {
                     openCamera()
                 }
             } else {
@@ -59,8 +69,20 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    fun getTimeStamp(): String {
+        return (System.currentTimeMillis() / 1000).toString();
+    }
+
+    @Suppress("DEPRECATION")
     private fun openCamera() {
-        TODO("Not yet implemented")
+        val values = ContentValues()
+        values.put(MediaStore.Images.Media.TITLE, getTimeStamp())
+        values.put(MediaStore.Images.Media.DESCRIPTION, "Picture from camera")
+        image_uri = contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values)
+
+        val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, image_uri)
+        startActivityForResult(cameraIntent, OPEN_CAMERA_CODE)
     }
 
     override fun onRequestPermissionsResult(
@@ -80,10 +102,11 @@ class MainActivity : AppCompatActivity() {
 
             PERMISSION_CODE_CAMERA_CAPTURE -> {
                 if (grantResults.size > 1 &&
-                        grantResults[0] == PackageManager.PERMISSION_GRANTED &&
-                        grantResults[1] == PackageManager.PERMISSION_GRANTED){
+                    grantResults[0] == PackageManager.PERMISSION_GRANTED &&
+                    grantResults[1] == PackageManager.PERMISSION_GRANTED
+                ) {
                     openCamera()
-                }else{
+                } else {
                     Toast.makeText(this, "Permissão negada!", Toast.LENGTH_SHORT).show()
                 }
             }
@@ -103,6 +126,9 @@ class MainActivity : AppCompatActivity() {
         super.onActivityResult(requestCode, resultCode, data)
         if (resultCode == Activity.RESULT_OK && requestCode == IMAGE_PICK_CODE) {
             binding.ivImage.setImageURI(data?.data)
+        }
+        if (resultCode == Activity.RESULT_OK && requestCode == OPEN_CAMERA_CODE) {
+            binding.ivImage.setImageURI(image_uri)
         }
     }
 }
