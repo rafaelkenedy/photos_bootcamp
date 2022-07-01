8b973ad6afc9efea46e3cc6c2f67ffed3cc50aa9
rafaelkenedy <k.rafaelalves@gmail.com>
2022-07-01 03:18:34 -0300

    Finished

--- app/src/main/java/com/rafael/photosbootcamp/MainActivity.kt
+++ app/src/main/java/com/rafael/photosbootcamp/MainActivity.kt
@@ -1,14 +1,78 @@
 package com.rafael.photosbootcamp
 
+import android.Manifest
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import com.rafael.photosbootcamp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
 
+        binding.btnPick.setOnClickListener {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+                if(checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
+                == PackageManager.PERMISSION_DENIED){
+                    val permission = arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE)
+                    requestPermissions(permission, PERMISSION_CODE)
+                }
+                else{
+                    pickImageFromGallery()
+                }
+            }else{
+                pickImageFromGallery()
+            }
+        }
+    }
 
-        
+    override fun onRequestPermissionsResult( requestCode: Int,
+                                             permissions: Array<out String>,
+                                             grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when(requestCode) {
+            PERMISSION_CODE -> {
+                if(grantResults.size > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    pickImageFromGallery()
+                }
+                else{
+                    Toast.makeText(this, "Permissão negada!", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+
+    }
+
+    @Suppress("DEPRECATION")
+    private fun pickImageFromGallery() {
+        val intent = Intent(Intent.ACTION_PICK)
+        intent.type = "image/*"
+        startActivityForResult(intent, IMAGE_PICK_CODE)
+    }
+
+    @Suppress("DEPRECATION")
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == Activity.RESULT_OK && requestCode == IMAGE_PICK_CODE){
+            binding.ivImage.setImageURI(data?.data)
+        }
+    }
+
+    companion object {
+        private val PERMISSION_CODE = 1000
+        private val IMAGE_PICK_CODE = 1001
+
     }
 }
